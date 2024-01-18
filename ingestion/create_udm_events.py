90886c15f33064cf3f1838770f82ab9cf244fdb1
Dan Dye <dandye@google.com>
2024-01-18 14:53:36 -0500

    add v1alpha create_udm_events

--- ingestion/create_udm_events.py
+++ ingestion/create_udm_events.py
@@ -32,7 +32,7 @@ array of JSON formatted events in UDM format. See
 ./example_input/sample_udm_events.json for an example.
 
 So, assuming you've created a credentials file at ~/.chronicle_credentials.json,
-you can run this command using the sample imput like so:
+you can run this command using the sample input like so:
 
 $ create_udm_events --customer_id=<your customer UUID> \
   --json_events_file=./example_input/sample_udm_events.json
@@ -55,47 +55,51 @@ INGESTION_API_BASE_URL = "https://malachiteingestion-pa.googleapis.com"
 AUTHORIZATION_SCOPES = ["https://www.googleapis.com/auth/malachite-ingestion"]
 
 
-def create_udm_events(http_session: requests.AuthorizedSession,
-                      customer_id: str, json_events: str) -> None:
-  """Sends a collection of UDM events to the Chronicle backend for ingestion.
+def create_udm_events(
+    http_session: requests.AuthorizedSession, customer_id: str, json_events: str
+) -> None:
+    """Sends a collection of UDM events to the Chronicle backend for ingestion.
 
-  A Unified Data Model (UDM) event is a structured representation of an event
-  regardless of the log source.
+    A Unified Data Model (UDM) event is a structured representation of an event
+    regardless of the log source.
 
-  Args:
-    http_session: Authorized session for HTTP requests.
-    customer_id: A string containing the UUID for the Chronicle customer.
-    json_events: A collection of UDM events in (serialized) JSON format.
+    Args:
+      http_session: Authorized session for HTTP requests.
+      customer_id: A string containing the UUID for the Chronicle customer.
+      json_events: A collection of UDM events in (serialized) JSON format.
 
-  Raises:
-    requests.exceptions.HTTPError: HTTP request resulted in an error
-      (response.status_code >= 400).
-  """
-  url = f"{INGESTION_API_BASE_URL}/v2/udmevents:batchCreate"
-  body = {
-      "customerId": customer_id,
-      "events": json.loads(json_events),
-  }
+    Raises:
+      requests.exceptions.HTTPError: HTTP request resulted in an error
+        (response.status_code >= 400).
+    """
+    url = f"{INGESTION_API_BASE_URL}/v2/udmevents:batchCreate"
+    body = {
+        "customerId": customer_id,
+        "events": json.loads(json_events),
+    }
 
-  response = http_session.request("POST", url, json=body)
-  response.raise_for_status()
+    response = http_session.request("POST", url, json=body)
+    response.raise_for_status()
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  chronicle_auth.add_argument_credentials_file(parser)
-  regions.add_argument_region(parser)
-  parser.add_argument(
-      "--customer_id", type=str, required=True, help="the customer UUID")
-  parser.add_argument(
-      "--json_events_file",
-      type=argparse.FileType("r"),
-      required=True,
-      help="path to a file (or \"-\" for STDIN) containing a list of UDM "
-      "events in json format")
+    parser = argparse.ArgumentParser()
+    chronicle_auth.add_argument_credentials_file(parser)
+    regions.add_argument_region(parser)
+    parser.add_argument(
+        "--customer_id", type=str, required=True, help="the customer UUID"
+    )
+    parser.add_argument(
+        "--json_events_file",
+        type=argparse.FileType("r"),
+        required=True,
+        help='path to a file (or "-" for STDIN) containing a list of UDM '
+        "events in json format",
+    )
 
-  args = parser.parse_args()
-  INGESTION_API_BASE_URL = regions.url(INGESTION_API_BASE_URL, args.region)
-  session = chronicle_auth.initialize_http_session(
-      args.credentials_file, scopes=AUTHORIZATION_SCOPES)
-  create_udm_events(session, args.customer_id, args.json_events_file.read())
+    args = parser.parse_args()
+    INGESTION_API_BASE_URL = regions.url(INGESTION_API_BASE_URL, args.region)
+    session = chronicle_auth.initialize_http_session(
+        args.credentials_file, scopes=AUTHORIZATION_SCOPES
+    )
+    create_udm_events(session, args.customer_id, args.json_events_file.read())
