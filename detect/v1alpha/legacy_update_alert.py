8368db707206bf896755650cc4f3cae2f6ea0856
Dan Dye <dandye@google.com>
2024-07-02 11:24:56 -0400

    code cleanup

--- detect/v1alpha/legacy_update_alert.py
+++ detect/v1alpha/legacy_update_alert.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import argparse
 import json
-
-from google.auth.transport import requests
+from typing import Dict
 
 from common import chronicle_auth
 from common import project_id
 from common import project_instance
 from common import regions
+
+from google.auth.transport import requests
 
 SCOPES = [
     "https://www.googleapis.com/auth/cloud-platform",
@@ -62,8 +77,8 @@ def legacy_update_alert(
     proj_instance: str,
     proj_region: str,
     alert_id: str,
-    feedback: dict=None,
-) -> str:
+    feedback: Dict[str, str]=None,
+    ) -> Dict[str, any]:
   """Legacy Update an Alert.
 
   Args:
@@ -72,7 +87,7 @@ def legacy_update_alert(
     proj_instance: Customer ID (uuid with dashes) for the Chronicle instance.
     proj_region: region in which the target project is located.
     alert_id: identifier for the alert to be closed.
-    feedback: dictionary containing the feedback key/values.
+    feedback: (optional) dictionary containing the feedback key/values.
 
   Returns:
     response.json
@@ -132,12 +147,14 @@ if __name__ == "__main__":
   regions.add_argument_region(parser)
   # local
   group = parser.add_mutually_exclusive_group(required=True)
-  group.add_argument("--alert_id",
-                      type=str,
-                      help="ID of the Alert.")
-  group.add_argument("--alert_ids_file",
-                      type=str,
-                      help="File with one Alert ID per line.")
+  group.add_argument(
+      "--alert_id",
+      type=str,
+      help="ID of the Alert.")
+  group.add_argument(
+      "--alert_ids_file",
+      type=str,
+      help="File with one Alert ID per line.")
   args = parser.parse_args()
 
   # pylint: disable-next=line-too-long
@@ -156,7 +173,7 @@ if __name__ == "__main__":
     print(json.dumps(response, indent=2))
   elif args.alert_ids_file:
     with open(args.alert_ids_file) as fh:
-      for an_id in fh.readlines():
+      for an_id in fh:
         print(f"working on alert id: {an_id}")
         response = legacy_update_alert(
             auth_session,
